269ffc219c982c8f10e795524dfbb8f62de95f2d
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2023-03-09 10:09:04 -0500

    rlang fix #11

--- function_plot_crossection.R
+++ function_plot_crossection.R
@@ -3,7 +3,7 @@ plot_crossection <- function(las,
                              p2 = c(max(las@data$X), mean(las@data$Y)),
                              width = 4, colour_by = NULL)
 {
-  colour_by <- enquo(colour_by)
+  colour_by <- rlang::enquo(colour_by)
   data_clip <- clip_transect(las, p1, p2, width)
   p <- ggplot(data_clip@data, aes(X,Z)) + geom_point(size = 0.5) + coord_equal() + theme_minimal()
 
